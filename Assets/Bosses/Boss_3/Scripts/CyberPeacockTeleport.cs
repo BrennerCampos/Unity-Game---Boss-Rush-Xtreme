364b51f84b277d8e6ad38196ebdea9a2df2403bc
Brenner Campos <brennercampos@gmail.com>
2021-11-12 13:34:11 -0500

    fdsfsfffwef

--- Assets/Bosses/Boss_3/Scripts/CyberPeacockTeleport.cs
+++ Assets/Bosses/Boss_3/Scripts/CyberPeacockTeleport.cs
@@ -8,67 +8,40 @@ using UnityEngine;
 public class CyberPeacockTeleport : EnemyAction
 {
 
-    public GameObject GroundDust, mainCamera;
-    //public CameraShake cameraShake;
+    public GameObject mainCamera;
     public CameraShake cam;
 
-    public float horizontalForce = 1f;
-    public float jumpForce = 2.0f;
+    public float horizontalForce = 0f;
+    public float verticalForce = 0f;
 
-    public float buildupTime;
-    public float teleportTimeMin, teleportTimeMax;
 
-    public string animationTriggerName;
-    public bool shakeCameraOnLanding;
-
-    private float teleportTime;
-    private bool isGrounded, hasTeleported;
+    private bool isGrounded, hasCompletedTask;
 
     private Tween buildupTween;
-    private Tween teleportTween;
+    private Tween jumpTween;
 
     public override void OnStart()
     {
-        teleportTime = Random.Range(teleportTimeMin, teleportTimeMax);
-        
-        buildupTween = DOVirtual.DelayedCall(buildupTime, StartTeleport, false);
-        animator.SetTrigger(animationTriggerName);
-        // cam = mainCamera.GetComponent<CameraShake>();
-        animator.ResetTrigger("isGrounded");
-        animator.SetBool("isTeleporting", true);
 
-    }
-
-    public void StartTeleport()
-    {
-        var direction = PlayerController.instance.transform.position.x < transform.position.x ? -1 : 1;
-
-        teleportTween = DOVirtual.DelayedCall(teleportTime, () =>
-        {
-            transform.position = new Vector2(Random.Range(PlayerController.instance.transform.position.x - 2, 
-                                                              PlayerController.instance.transform.position.x + 2), 
-                                                                                            transform.position.y);
-            hasTeleported = true;
-        }, false);
     }
 
 
     public override TaskStatus OnUpdate()
     {
-        
-        // TELEPORTING
-        
-        return hasTeleported ? TaskStatus.Success : TaskStatus.Running;
+        return hasCompletedTask ? TaskStatus.Success : TaskStatus.Running;
     }
 
 
     public override void OnEnd()
     {
-        buildupTween?.Kill();
-        teleportTween?.Kill();
-        hasTeleported = false;
-        animator.SetBool("isTeleporting", false);
+        hasCompletedTask = false;
     }
 
+
+    public void InsertFunctionHere()
+    {
+        var direction = PlayerController.instance.transform.position.x < transform.position.x ? -1 : 1;
+
+    }
 
 }
